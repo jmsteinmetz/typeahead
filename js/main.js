0905e18937255f592348d888f3398617dcd5673a
John Steinmetz <john@slingspace.com>
2016-10-19 08:38:16 -0500

    Added Recent Entries

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+$(function() {
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
 
+
+    var placesAPI = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: '/data/places.json?x='+randomString(5)
+    });
+
+    $('#custom-templates #searchKeywords').typeahead(null, {
+        name: 'show-places',
+        display: 'value',
+        source: placesAPI,
+        hint: true,
+		highlight: true,
+		minLength: 1,
+        templates: {
+            empty: [
+                '<div class="empty-message">',
+                'Unable to find any data that matches the current query',
+                '</div>'
+            ].join('\n'),
+            suggestion: Handlebars.compile('<div><span class="name"><i class="icon-{{type}} suggestion" aria-hidden="true"></i> {{value}}</span></div>')
+        	
+        }
+    });
+
+    function randomString(len, charSet) {
+        charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var randomString = '';
+        for (var i = 0; i < len; i++) {
+            var randomPoz = Math.floor(Math.random() * charSet.length);
+            randomString += charSet.substring(randomPoz, randomPoz + 1);
+        }
+        return randomString;
+    }
+
+
+
+});
